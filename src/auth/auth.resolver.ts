3f0cf3a8d970cebca1b7e703fa85c8e5706d495f
Cesar-Marcano <marcanocesar86@gmail.com>
2025-01-21 22:04:55 -0400

    feat: implement Auth module

---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,12 @@
+import { Query, Resolver } from '@nestjs/graphql';
+// import { AuthService } from './auth.service';
+
+@Resolver()
+export class AuthResolver {
+  // constructor(private readonly authService: AuthService) {}
+
+  @Query(() => String)
+  sayHello(): string {
+    return 'Hello World!';
+  }
+}
