ec7da927f65ca098337be2e2d00dbb56c1ef7500
Cesar-Marcano <marcanocesar86@gmail.com>
2025-01-22 17:17:23 -0400

    feat: disable 'x-powered-by' header for enhanced security

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { HttpAdapterHost } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  httpAdapter.getInstance().disable('x-powered-by');
+
   app.useGlobalPipes(new ValidationPipe());
+  
   await app.listen(process.env['PORT'] ?? 3000);
 }
 bootstrap();
