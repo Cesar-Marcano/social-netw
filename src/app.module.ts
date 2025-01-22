78b35306bacd6f8f95a8a89443b85324850166e1
Cesar-Marcano <marcanocesar86@gmail.com>
2025-01-21 22:04:25 -0400

    feat: add global validation pipe and update dependencies for Apollo and GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,26 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule, MongooseModuleFactoryOptions } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 @Module({
   imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
     ConfigModule.forRoot({ isGlobal: true }),
     MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
       useFactory: async (
         configService: ConfigService,
       ): Promise<MongooseModuleFactoryOptions> => ({
         uri:
           configService.get<string>('MONGO_URI') ??
-          'http://localhost:27017/test',
+          'mongodb://localhost:27017/test',
       }),
-      inject: [ConfigModule],
     }),
     UserModule,
   ],
