c70186a90ac06a8cbe7fc5516996a20e22406abe
Cesar-Marcano <marcanocesar86@gmail.com>
2025-01-22 17:51:27 -0400

    feat: set content security policy

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { HttpAdapterHost } from '@nestjs/core';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,8 +10,19 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   httpAdapter.getInstance().disable('x-powered-by');
 
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          frameAncestors: ["'self'"],
+        },
+      },
+    }),
+  );
+
   app.useGlobalPipes(new ValidationPipe());
-  
+
   await app.listen(process.env['PORT'] ?? 3000);
 }
 bootstrap();
