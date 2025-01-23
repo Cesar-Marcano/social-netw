120ced9b6fcc30a0fd21f36f4e546c581bf27a5a
Cesar-Marcano <marcanocesar86@gmail.com>
2025-01-23 19:02:20 -0400

    chore: integrate Mongoose for Comment schema in CommentModule

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Comment, CommentSchema } from './comment.schema';
 
 @Module({
-  providers: [CommentService]
+  imports: [
+    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
+  ],
+  providers: [CommentService],
 })
 export class CommentModule {}
