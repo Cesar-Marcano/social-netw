db5e42196d5a1bf6f9b165462020b19e84190d52
Cesar-Marcano <marcanocesar86@gmail.com>
2025-01-23 19:00:32 -0400

    feat: add Comment schema

---
+++ src/comment/comment.schema.ts
@@ -0,0 +1,27 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { MongooseTimestamp } from '../common/types/mongoose-timestamp.type';
+
+export type CommentDocument = HydratedDocument<Comment> & MongooseTimestamp;
+
+@ObjectType()
+@Schema({ timestamps: true })
+export class Comment {
+  @Field(() => ID)
+  id?: mongoose.Types.ObjectId; 
+
+  @Prop({ required: true, ref: 'User', type: mongoose.Types.ObjectId })
+  @Field(() => String)
+  author!: mongoose.Types.ObjectId;
+
+  @Prop({ required: true, ref: 'Post', type: mongoose.Types.ObjectId })
+  @Field(() => String)
+  post!: mongoose.Types.ObjectId;
+
+  @Prop({ required: true, min: 1, max: 1024 })
+  @Field()
+  content!: string;
+}
+
+export const CommentSchema = SchemaFactory.createForClass(Comment);
