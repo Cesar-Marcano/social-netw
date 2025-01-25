52b8ec356603834560289472a3fdb4ba5ce23337
Cesar-Marcano <marcanocesar86@gmail.com>
2025-01-25 11:04:54 -0400

    feat: implement ReactionService to handle reactions for posts and comments

--- src/reaction/reaction.module.ts
+++ src/reaction/reaction.module.ts
@@ -1,12 +1,20 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Reaction, ReactionSchema } from './reaction.schema';
+import { ReactionService } from './reaction.service';
+import { PostModule } from 'src/post/post.module';
+import { CommentModule } from 'src/comment/comment.module';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Reaction.name, schema: ReactionSchema },
     ]),
+    PostModule,
+    CommentModule,
+    UserModule,
   ],
+  providers: [ReactionService],
 })
 export class ReactionModule {}
---
+++ src/reaction/reaction.service.ts
@@ -0,0 +1,47 @@
+import { Model, Types } from 'mongoose';
+import { CommentService } from 'src/comment/comment.service';
+import { PostService } from 'src/post/post.service';
+
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Reaction, ReactionDocument } from './reaction.schema';
+import { ReactionTarget } from './types/reaction-target.enum';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class ReactionService {
+  constructor(
+    @InjectModel(Reaction.name) private readonly reactionModel: Model<Reaction>,
+    private readonly postService: PostService,
+    private readonly commentService: CommentService,
+    private readonly userService: UserService,
+  ) {}
+
+  /**
+   * Adds a reaction to a target (post or comment).
+   *
+   * @param {Reaction} reactionData - The reaction data, including the author, target, and reaction type.
+   * @returns {Promise<ReactionDocument>} - The created reaction document.
+   */
+  async addReaction(reactionData: Reaction): Promise<ReactionDocument> {
+    const reactionExists = await this.reactionModel.findOne({
+      author: reactionData.author,
+      targetId: reactionData.targetId,
+    });
+
+    if (reactionExists) {
+      throw new BadRequestException('You cannot react two times.');
+    }
+
+    this.userService.checkIfUserExists(reactionData.author);
+
+    if (reactionData.targetType === ReactionTarget.POST) {
+      this.postService.checkIfPostExists(reactionData.targetId);
+    } else if (reactionData.targetType === ReactionTarget.COMMENT) {
+      this.commentService.checkIfCommentExists(reactionData.targetId);
+    }
+
+    return this.reactionModel.create(reactionData);
+  }
+}
